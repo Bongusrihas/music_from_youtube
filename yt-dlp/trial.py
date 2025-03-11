d29dd9b3b5536af7f2f177be9b96f5705be57d84
Bongusrihas <164666377+Bongusrihas@users.noreply.github.com>
2025-03-11 23:20:37 +0530

    Add files via upload

---
+++ yt-dlp/trial.py
@@ -0,0 +1,15 @@
+# import yt_dlp as ytd
+
+# url="https://youtu.be/1nPi_r5z_Mk?si=DlXHbmthOOlSqQVC"
+# options={
+#             'format': 'bestaudio/best',
+#             'postprocessors': [{
+#                 'key':'FFmpegExtractAudio',
+#                 'preferredcodec' :'mp3',
+#                 'preferredquality':'192',
+#             }],
+#         }
+# with ytd.YoutubeDL(options) as yld:
+#     yld.download([url])
+
+
